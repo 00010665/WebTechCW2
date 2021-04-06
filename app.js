061b86a273b25bed455324a48def62dd2c83e693
00010665 <student00010665@gmail.com>
2021-04-07 01:41:46 +0500

    First lesson
    App.js file created

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-.
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'pug')
+
+app.listen(PORT, err => {
+	if(err) throw err
+
+	console.log(`App is running on port ${ PORT } ...`)
+})
